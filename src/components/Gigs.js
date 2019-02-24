cc858ea642ea7712f5e7df85e79913fff764dede
Sam Banks <sam@stackchat.com>
2019-02-24 13:56:17 +1100

    initial gatsby scaffold

---
+++ src/components/Gigs.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import pic03 from '../images/pic03.jpg'
+
+const Gigs = () => (
+  <div>
+    <h2 className="major">Gigs</h2>
+    <span className="image main">
+      <img src={pic03} alt="" />
+    </span>
+    <p>
+      Fred ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
+      eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat
+      volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam.
+      Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et
+      malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit
+      amet ex mollis mattis lorem ipsum dolor sit amet.
+    </p>
+  </div>
+)
+
+export default Gigs
